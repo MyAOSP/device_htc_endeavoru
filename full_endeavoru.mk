d6d665193cc5ec67f0e7db08b3ff8f026def9306
travp624 <travp624@gmail.com>
2013-06-16 21:36:42 -0500

    cleanup endeavoru add PRODUCT_NAME
    Signed-off-by: travp624 <travp624@gmail.com>

--- full_endeavoru.mk
+++ full_endeavoru.mk
@@ -28,125 +28,124 @@ PRODUCT_LOCALES += en_US xhdpi
 
 # Init files
 PRODUCT_COPY_FILES := \
-	$(LOCAL_KERNEL):kernel \
-	$(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
-	$(LOCAL_PATH)/ramdisk/init.usb.rc:root/init.usb.rc \
-	$(LOCAL_PATH)/ramdisk/init.endeavoru.rc:root/init.endeavoru.rc \
-	$(LOCAL_PATH)/ramdisk/ueventd.endeavoru.rc:root/ueventd.endeavoru.rc \
-        $(LOCAL_PATH)/ramdisk/fstab.endeavoru.ext4:root/fstab.endeavoru.ext4 \
-        $(LOCAL_PATH)/ramdisk/fstab.endeavoru.vfat:root/fstab.endeavoru.vfat \
-        $(LOCAL_PATH)/ramdisk/fstab.endeavoru:root/fstab.endeavoru \
-        $(LOCAL_PATH)/ramdisk/wifi_loader.sh:root/wifi_loader.sh \
-        $(LOCAL_PATH)/ramdisk/init:root/init
+    $(LOCAL_KERNEL):kernel \
+    $(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
+    $(LOCAL_PATH)/ramdisk/init.usb.rc:root/init.usb.rc \
+    $(LOCAL_PATH)/ramdisk/init.endeavoru.rc:root/init.endeavoru.rc \
+    $(LOCAL_PATH)/ramdisk/ueventd.endeavoru.rc:root/ueventd.endeavoru.rc \
+    $(LOCAL_PATH)/ramdisk/fstab.endeavoru.ext4:root/fstab.endeavoru.ext4 \
+    $(LOCAL_PATH)/ramdisk/fstab.endeavoru.vfat:root/fstab.endeavoru.vfat \
+    $(LOCAL_PATH)/ramdisk/fstab.endeavoru:root/fstab.endeavoru \
+    $(LOCAL_PATH)/ramdisk/wifi_loader.sh:root/wifi_loader.sh \
+    $(LOCAL_PATH)/ramdisk/init:root/init
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-	$(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-	$(LOCAL_PATH)/configs/hostapd.conf:system/etc/wifi/hostapd.conf \
-	$(LOCAL_PATH)/configs/nvcamera.conf:system/etc/nvcamera.conf \
-	$(LOCAL_PATH)/configs/asound.conf:system/etc/asound.conf \
-	$(LOCAL_PATH)/configs/enctune.conf:system/etc/enctune.conf \
-	$(LOCAL_PATH)/configs/DSP_number.txt:system/etc/DSP_number.txt \
-	$(LOCAL_PATH)/configs/AIC3008_REG_DualMic_XC.csv:system/etc/AIC3008_REG_DualMic_XC.csv \
-	$(LOCAL_PATH)/configs/AIC3008_REG_DualMic.csv:system/etc/AIC3008_REG_DualMic.csv \
-	$(LOCAL_PATH)/configs/alsa.conf:system/usr/share/alsa/alsa.conf
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/configs/hostapd.conf:system/etc/wifi/hostapd.conf \
+    $(LOCAL_PATH)/configs/nvcamera.conf:system/etc/nvcamera.conf \
+    $(LOCAL_PATH)/configs/asound.conf:system/etc/asound.conf \
+    $(LOCAL_PATH)/configs/enctune.conf:system/etc/enctune.conf \
+    $(LOCAL_PATH)/configs/DSP_number.txt:system/etc/DSP_number.txt \
+    $(LOCAL_PATH)/configs/AIC3008_REG_DualMic_XC.csv:system/etc/AIC3008_REG_DualMic_XC.csv \
+    $(LOCAL_PATH)/configs/AIC3008_REG_DualMic.csv:system/etc/AIC3008_REG_DualMic.csv \
+    $(LOCAL_PATH)/configs/alsa.conf:system/usr/share/alsa/alsa.conf
 
 # GPS Certificate
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/etc/SuplRootCert:system/etc/SuplRootCert
+    $(LOCAL_PATH)/prebuilt/etc/SuplRootCert:system/etc/SuplRootCert
 
 # Wifi
 PRODUCT_PACKAGES += \
-	TQS_D_1.7.ini \
-	calibrator \
-        hostapd \
-        hostapd_cli \
-        iw
+    TQS_D_1.7.ini \
+    calibrator \
+    hostapd \
+    hostapd_cli \
+    iw
 
 # Modules
 PRODUCT_COPY_FILES += \
-        $(foreach f,$(wildcard $(LOCAL_PATH)/prebuilt/modules/*),$(f):system/lib/modules/$(notdir $(f)))
+    $(foreach f,$(wildcard $(LOCAL_PATH)/prebuilt/modules/*),$(f):system/lib/modules/$(notdir $(f)))
 
 # Vold
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab
+    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab
 
 # video
 PRODUCT_PACKAGES += \
-        libstagefrighthw-htc
+    libstagefrighthw-htc
 
 # Lights
 PRODUCT_PACKAGES += \
-	lights.tegra
+    lights.tegra
 
 # Keyboard
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl
+    $(LOCAL_PATH)/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl
 
 # NFC
 PRODUCT_PACKAGES += \
-	libnfc \
-	libnfc_jni \
-	Nfc \
-	Tag \
-	com.android.nfc_extras \
-	com.android.future.usb.accessory
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag \
+    com.android.nfc_extras \
+    com.android.future.usb.accessory
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-	LiveWallpapers \
-	LiveWallpapersPicker \
-	VisualizationWallpapers \
-	librs_jni
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    librs_jni
 
 # Audio
 PRODUCT_PACKAGES += \
-	audio.a2dp.default \
-	libaudioutils \
-	libtinyalsa \
-        tinymix \
-        tinyplay \
-        tinycap
+    audio.a2dp.default \
+    libaudioutils \
+    libtinyalsa \
+    tinymix \
+    tinyplay \
+    tinycap
 
 # Pollyd
 PRODUCT_PACKAGES += \
-	pollyd \
-	Polly
+    pollyd \
+    Polly
 
 # hox tools
 PRODUCT_PACKAGES += \
-	ugexec \
-	libbt-vendor \
-	hox-uim-sysfs
+    ugexec \
+    libbt-vendor \
+    hox-uim-sysfs
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-	make_ext4fs \
-	setup_fs
+    make_ext4fs \
+    setup_fs
 
 # Custom Packages
 PRODUCT_PACKAGES += \
-        Music \
-        CMFileManager \
-        CellBroadcastReceiver \
-        Apollo \
-        DSPManager \
-        libcyanogen-dsp \
-        audio_effects.conf
+    Music \
+    CMFileManager \
+    CellBroadcastReceiver \
+    DSPManager \
+    libcyanogen-dsp \
+    audio_effects.conf
 
 # Increase the HWUI font cache
 PRODUCT_PROPERTY_OVERRIDES += \
-        ro.hwui.text_cache_width=2048 \
-        ro.hwui.text_cache_height=256
+    ro.hwui.text_cache_width=2048 \
+    ro.hwui.text_cache_height=256
 
 # Power
 PRODUCT_PACKAGES += \
-        power.endeavoru
+    power.endeavoru
 
 # init.d support
 PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/prebuilt/etc/post-boot.sh:system/etc/post-boot.sh \
-        $(LOCAL_PATH)/ramdisk/bin/sysinit:system/bin/sysinit
+    $(LOCAL_PATH)/prebuilt/etc/post-boot.sh:system/etc/post-boot.sh \
+    $(LOCAL_PATH)/ramdisk/bin/sysinit:system/bin/sysinit
 
 # GPU OC scripts
 PRODUCT_COPY_FILES += \
@@ -155,40 +154,40 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-        frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-        frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-        frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-        frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-        frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-        frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-        frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-        frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-        frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-        frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-        frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-        frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-        frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-        frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-        frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-        frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-        frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/usr/idc/atmel-maxtouch.idc:system/usr/idc/atmel-maxtouch.idc \
-	$(LOCAL_PATH)/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
-	$(LOCAL_PATH)/prebuilt/usr/idc/tv-touchscreen.idc:system/usr/idc/tv-touchscreen.idc
+    $(LOCAL_PATH)/prebuilt/usr/idc/atmel-maxtouch.idc:system/usr/idc/atmel-maxtouch.idc \
+    $(LOCAL_PATH)/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
+    $(LOCAL_PATH)/prebuilt/usr/idc/tv-touchscreen.idc:system/usr/idc/tv-touchscreen.idc
 
 # Custom media config for HTC camera
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=320 \
-	persist.sys.usb.config=mtp,adb \
-	ro.adb.secure=0 \
-        ro.product.manufacturer=HTC
+    ro.sf.lcd_density=320 \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.product.manufacturer=HTC
 
 $(call inherit-product-if-exists, hardware/ti/wan/mac80211/Android.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -199,4 +198,5 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 PRODUCT_DEVICE := endeavoru
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := HTC One X
+PRODUCT_NAME := HTC One x
 PRODUCT_MANUFACTURER := HTC
